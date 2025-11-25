39e384d3b718cfad9ed183e8dbd88347973b70a0
Shareef Evans <shareef3vans@gmail.com>
2025-11-26 08:15:51 +1100

    Initial Commit

---
+++ orie/orieApp.swift
@@ -0,0 +1,17 @@
+//
+//  orieApp.swift
+//  orie
+//
+//  Created by Shareef Evans on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct orieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
